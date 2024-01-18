52d9d4b81b5a47ba088f41f0a700731c3d08fbf5
washing1127 <1014585392@qq.com>
2024-01-18 22:52:25 +0800

    os.system

--- run_spider.py
+++ run_spider.py
@@ -1,5 +1,6 @@
 import os
 import json
+import shutil
 import subprocess
 import traceback
 
@@ -7,15 +8,14 @@ with open("repo_list.jsonl", "r", encoding="utf-8")as r:
     data = r.readlines()
 
 err_count = 0
-
+if not os.path.exists("output"): os.mkdir("output")
 for line in data:
     try:
         dic = json.loads(line.strip())
         clone_url = dic["git"].replace("//", "/").replace(":/", "://")
         repo_name = dic["name"]
         clone_path = os.path.join("output", repo_name)
-        os.makedirs(os.path.dirname(clone_path), exist_ok=True)
-        subprocess.run(['git', 'clone', clone_url, clone_path])
+        os.system(f"git clone {clone_url} {clone_path}")
     except: 
         traceback.print_exc()
         err_count += 1
